475f6eb5d4c4ecd195011274a8cbe9d436def557
Lenovo <Lenovo@mikasa.bbrouter>
2024-08-21 14:15:22 +0530

    this is my submission

--- src/test/java/Para_Pageobjects/Login_Page.java
+++ src/test/java/Para_Pageobjects/Login_Page.java
@@ -3,7 +3,7 @@ package Para_Pageobjects;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.testng.asserts.SoftAssert;
+
 
 
 public class Login_Page extends para_Base
---
+++ src/test/java/Para_Testcases/Dummy.java
@@ -0,0 +1,12 @@
+package Para_Testcases;
+
+public class Dummy 
+{
+    
+	public static void main(String[]args)
+	{
+		System.out.println("Saikrishna");
+		
+	}
+	
+}
